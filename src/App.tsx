1749075ee933217e01369b39d6a224de2695ccc2
Nicholas-Easter <nicholas@airifoundation.org>
2026-07-10 15:46:15 -0600

    Reordered the imports by alphabetical order

--- src/App.tsx
+++ src/App.tsx
@@ -1,81 +1,80 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import FounderPortal from "./pages/FounderPortal";
-import SignUp from "./pages/SignUp";
-import VerifyOtp from "./pages/VerifyOtp";
-import BuildCompanyProfile from "./pages/BuildCompanyProfile";
+
 import AuthenticationSuccess from "./pages/AuthenticationSuccess";
-import PasswordResetSuccess from "./pages/PasswordResetSuccess";
-import Error from "./pages/Error";
-import Login from "./pages/Login";
-import Pricing from "./pages/Pricing";
+import BookDemo from "./pages/BookDemo";
+import BuildCompanyProfile from "./pages/BuildCompanyProfile";
+import Contact from "./pages/Contact";
 import DashboardMilestones from "./pages/DashboardMilestones";
 import DashboardProfile from "./pages/DashboardProfile";
+import DashboardSettingsBilling from "./pages/DashboardSettingsBilling";
+import DashboardSettingsNotifications from "./pages/DashboardSettingsNotifications";
 import DashboardSettingsProfile from "./pages/DashboardSettingsProfile";
 import DashboardSettingsSecurity from "./pages/DashboardSettingsSecurity";
-import DashboardSettingsNotifications from "./pages/DashboardSettingsNotifications";
-import DashboardSettingsBilling from "./pages/DashboardSettingsBilling";
-import SendPasswordResetOtp from "./pages/sendPasswordResetOtp";
-import PasswordReset from "./pages/PasswordReset";
-import Contact from "./pages/Contact";
-import BookDemo from "./pages/BookDemo";
-import SubmissionSent from "./pages/SubmissionSent";
 import Directory from "./pages/Directory";
+import Error from "./pages/Error";
+import FounderPortal from "./pages/FounderPortal";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import PasswordReset from "./pages/PasswordReset";
+import PasswordResetSuccess from "./pages/PasswordResetSuccess";
+import Pricing from "./pages/Pricing";
+import SendPasswordResetOtp from "./pages/sendPasswordResetOtp";
+import SignUp from "./pages/SignUp";
+import SubmissionSent from "./pages/SubmissionSent";
+import VerifyOtp from "./pages/VerifyOtp";
 
-import DashboardAdvisoryProgram from "./pages/StubPages/DashboardAdvisoryProgram";
 import AboutUs from "./pages/StubPages/AboutUs";
-import Product from "./pages/StubPages/Product";
+import Advisory from "./pages/StubPages/Advisory";
+import CompanyProfile from "./pages/StubPages/CompanyProfile";
+import DashboardAdvisoryProgram from "./pages/StubPages/DashboardAdvisoryProgram";
+import InvestorHub from "./pages/StubPages/InvestorHub";
 import LearnMoreCorporations from "./pages/StubPages/LearnMoreCorporations";
 import LearnMoreFounders from "./pages/StubPages/LearnMoreFounders";
 import LearnMoreGovernment from "./pages/StubPages/LearnMoreGovernment";
 import LearnMoreInvestors from "./pages/StubPages/LearnMoreInvestors";
-import CompanyProfile from "./pages/StubPages/CompanyProfile";
-import Spectrum from "./pages/StubPages/Spectrum";
-import Advisory from "./pages/StubPages/Advisory";
-import InvestorHub from "./pages/StubPages/InvestorHub";
 import ManageSubscriptions from "./pages/StubPages/ManageSubscriptions";
-
+import Product from "./pages/StubPages/Product";
+import Spectrum from "./pages/StubPages/Spectrum";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/dashboard/overview" element={<FounderPortal />} />   
-        <Route path="/signup" element={<SignUp />} />   
-        <Route path="/verify-otp" element={<VerifyOtp />} />
+        <Route path="/authentication-success" element={<AuthenticationSuccess />}/>       
+        <Route path="/book-demo" element={<BookDemo />} />         
         <Route path="/build-company-profile" element={<BuildCompanyProfile />}/>
-        <Route path="/authentication-success" element={<AuthenticationSuccess />}/>
-        <Route path="/password-success" element={<PasswordResetSuccess />} />
-        <Route path="/error" element={<Error />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/contact" element={<Contact />} />   
         <Route path="/dashboard/milestone-tracker" element={<DashboardMilestones />} />
-        <Route path="/dashboard/milestone-tracker/:stage" element={<DashboardMilestones />}/>
-        <Route path="/dashboard/profile" element={<DashboardProfile />} />
-        <Route path="/dashboard/settings-profile" element={<DashboardSettingsProfile />}/>
-        <Route path="/dashboard/settings-security" element={<DashboardSettingsSecurity />}/>
-        <Route path="/dashboard/settings-notifications" element={<DashboardSettingsNotifications />}/>
+        <Route path="/dashboard/milestone-tracker/:stage" element={<DashboardMilestones />}/> 
+        <Route path="/dashboard/profile" element={<DashboardProfile />} />      
         <Route path="/dashboard/settings-billing" element={<DashboardSettingsBilling />}/>
-        <Route path="/password-reset-otp" element={<SendPasswordResetOtp />} />
-        <Route path="/password-reset" element={<PasswordReset />} />
-        <Route path="/contact" element={<Contact />} /> 
-        <Route path="/book-demo" element={<BookDemo />} />
-        <Route path="/submission-sent" element={<SubmissionSent />} />
-        <Route path="/directory" element={<Directory/>} />   
+        <Route path="/dashboard/settings-notifications" element={<DashboardSettingsNotifications />}/> 
+        <Route path="/dashboard/settings-profile" element={<DashboardSettingsProfile />}/>  
+        <Route path="/dashboard/settings-security" element={<DashboardSettingsSecurity />}/> 
+        <Route path="/directory" element={<Directory/>} />                                  
+        <Route path="/error" element={<Error />} />  
+        <Route path="/dashboard/overview" element={<FounderPortal />} />                                            
+        <Route path="/" element={<Home />} /> 
+        <Route path="/login" element={<Login />} />  
+        <Route path="/password-reset" element={<PasswordReset />} />    
+        <Route path="/password-success" element={<PasswordResetSuccess />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/password-reset-otp" element={<SendPasswordResetOtp />} />                                  
+        <Route path="/signup" element={<SignUp />} /> 
+        <Route path="/submission-sent" element={<SubmissionSent />} />          
+        <Route path="/verify-otp" element={<VerifyOtp />} />
 
-        <Route path="/dashboard/advisory-program" element={<DashboardAdvisoryProgram />}/>
-        <Route path="/about-us" element={<AboutUs/>} />   
-        <Route path="/product" element={<Product/>} />  
+        <Route path="/about-us" element={<AboutUs/>} /> 
+        <Route path="/advisory" element={<Advisory />} />        
+        <Route path="/company/:id" element={<CompanyProfile />} />        
+        <Route path="/dashboard/advisory-program" element={<DashboardAdvisoryProgram />}/>  
+        <Route path="/investor-hub" element={<InvestorHub />} />
         <Route path="/learn-corporations" element={<LearnMoreCorporations />} />
         <Route path="/learn-founders" element={<LearnMoreFounders />} />
         <Route path="/learn-government" element={<LearnMoreGovernment />} />
-        <Route path="/learn-investors" element={<LearnMoreInvestors />} />
-        <Route path="/directory" element={<Directory />} />
-        <Route path="/company/:id" element={<CompanyProfile />} />
-        <Route path="/spectrum" element={<Spectrum />} />
-        <Route path="/advisory" element={<Advisory />} />
-        <Route path="/investor-hub" element={<InvestorHub />} />  
-        <Route path="/manage-subscriptions" element={<ManageSubscriptions />} />                                 
+        <Route path="/learn-investors" element={<LearnMoreInvestors />} />    
+        <Route path="/manage-subscriptions" element={<ManageSubscriptions />} />                      
+        <Route path="/product" element={<Product/>} />  
+        <Route path="/spectrum" element={<Spectrum />} />                                 
       </Routes>
     </BrowserRouter>
   );
